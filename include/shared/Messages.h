86e043dc5912f5afea8cb88b1364b420c299c27c
BlastermanR <ryanbmassie@gmail.com>
2026-03-31 00:23:05 -0400

    serial comms update

---
+++ include/shared/Messages.h
@@ -0,0 +1,65 @@
+/**
+ * Messages.h
+ * @brief Shared message types for inter-core queue communication.
+ *
+ * @author Ryan Massie (rmassie)
+ * @date 3/31/26
+ */
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+#include <stdint.h>
+
+#define OUTPUT_MSG_TEXT_LEN 64
+#define COMMAND_QUEUE_SIZE  16
+#define OUTPUT_QUEUE_SIZE   32
+
+/**
+ * @brief Identifiers for inbound commands received over USB.
+ */
+enum CommandId : uint8_t
+{
+    Cmd_None = 0,
+    Cmd_StartProcess,
+    Cmd_StopProcess,
+    Cmd_PressurizeChamber,
+    Cmd_VentChamber,
+    Cmd_ShutOffGas,
+    Cmd_PollDevices,
+    Cmd_SetArgonFlow,
+    Cmd_SetOxygenFlow,
+    Cmd_SetPumpSpeed,
+    Cmd_EnablePump,
+    Cmd_DisablePump,
+    Cmd_Exit,
+};
+
+/**
+ * @brief Fixed-size command message pushed into the command queue.
+ */
+struct CommandMessage
+{
+    CommandId id;
+    float     param1;
+    float     param2;
+};
+
+/**
+ * @brief Identifies which core originated an output message.
+ */
+enum MessageSource : uint8_t
+{
+    Source_Core0 = 0,
+    Source_Core1 = 1,
+};
+
+/**
+ * @brief Fixed-size output message pushed into the output queues.
+ */
+struct OutputMessage
+{
+    MessageSource source;
+    char          text[OUTPUT_MSG_TEXT_LEN];
+};
+
+#endif // MESSAGES_H
